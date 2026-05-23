7a4000f23d243b0cb24fbe2eb006c92e60008b74
Hashim2266 <malaimh@jakim22.onmicrosoft.com>
2026-05-24 04:39:26 +0800

    Project updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>SKU System</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './skuApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
